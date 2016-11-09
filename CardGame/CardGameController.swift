8939663e66307894694e9fa0108fdbb809bb3df0
GearShift24 <jwri6847@740s-pl-369.local>
2016-11-09 08:31:48 -0700

    created a shuffle array

--- CardGame/CardGameController.swift
+++ CardGame/CardGameController.swift
@@ -9,11 +9,28 @@
 import UIKit
 class CardGameController : UIViewController
 {
-    override func viewDidLoad()
+
+    @IBOutlet weak var cardLabel: UILabel!
+
+    @IBOutlet weak var cardButton: UIButton!
+    
+    
+        private lazy var currentDeck = PlayingCardDeck()
+    
+        private lazy var clickCount = Int()
+    
+    
+      override func viewDidLoad()
     {
         let tempCard = Card()
         print(tempCard.toString())
         let otherTemp = PlayingCard()
         print(otherTemp.toString())
+    }
+    @IBAction func flipCard(sender: UIButton)
+    {
+        clickCount += 1
+        let words = "the card has been clicked \(clickCount) times"
+        cardLabel.text = words
     }
 }
--- CardGame/Model/Deck.swift
+++ CardGame/Model/Deck.swift
@@ -12,9 +12,34 @@ class Deck
 {
     lazy var cards = [Card]()
     
+    
     func shuffleDeck() -> Void
     {
+        var tempDeck = [Card]()
         
+        while cards.count > 0
+        {
+            //chooses a valid raindex in the range of 0...cards.count-1
+            let randomSpot = Int (arc4random() % UInt32(cards.count))
+            //takes the card from that spot, from the deck. the deck decreases in count.
+            let removedCard = cards.removeAtIndex(randomSpot)
+            //adds the removed card to the end of the new temporary deck.
+            tempDeck.append(removedCard)
+        }
+        //Add the removed card th the temporary deck
+        cards = tempDeck
     }
+    
+    
+    
+//    func drawCard() -> Card!
+//    {
+//        
+//    }
+//    
+//    func drawRandomCard() -> Card!
+//    {
+//        
+//    }
     
 }
