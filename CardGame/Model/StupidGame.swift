a3cf3a9d3b9dfbe10446cc39db83debccaa1d04f
GearShift24 <jwri6847@740s-pl-369.local>
2016-11-11 09:06:02 -0700

    tried to make game but failed

--- CardGame/Model/StupidGame.swift
+++ CardGame/Model/StupidGame.swift
@@ -5,5 +5,35 @@
 //  Created by Wrigley, Joseph on 11/11/16.
 //  Copyright © 2016 Ctec. All rights reserved.
 //
-
+import UIKit
 import Foundation
+
+class StupidGame
+{
+    //Declaration section
+    
+    internal var score : Int
+    internal var discardPile : UIImage
+    internal var winnersPile: UIImage
+    internal var drawingDeck : UIImage
+    internal var hand : UIImage
+    
+    
+    //inits
+    init()
+    {
+    self.score = 0
+    self.discardPile = UIImage()
+    self.winnersPile = UIImage()
+    self.drawingDeck = UIImage()
+    self.hand = UIImage()
+        
+    }
+    
+    
+    //methods
+    func startGame() -> Void
+    {
+        
+    }
+}
