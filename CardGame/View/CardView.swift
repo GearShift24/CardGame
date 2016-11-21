237564623abd6512bb4924ca1f13c7ff5b838880
GearShift24 <jwri6847@740s-pl-369.local>
2016-11-21 09:55:12 -0700

    added cardvoew and innitiated its vars

---
+++ CardGame/View/CardView.swift
@@ -0,0 +1,38 @@
+//
+//  CardView.swift
+//  CardGame
+//
+//  Created by Wrigley, Joseph on 11/21/16.
+//  Copyright © 2016 Ctec. All rights reserved.
+//
+
+import UIKit
+
+class CardView
+{
+    internal var isFaceUp : Bool
+    internal var cardBack : UIImageView
+    internal var cardFront : UIImageView
+    internal var topLabel : UILabel
+    internal var bottomLabel : UILabel
+    internal var cornerCurve : CGFloat
+    
+    
+    init()
+    {
+        isFaceUp = false
+        cardBack = UIImageView()
+        cardFront = UIImageView()
+        topLabel = UILabel()
+        bottomLabel = UILabel()
+        cornerCurve = CGFloat (20.00)
+    }
+    
+    
+    func setup() -> Void
+    {
+  
+    }
+}
+
+    
